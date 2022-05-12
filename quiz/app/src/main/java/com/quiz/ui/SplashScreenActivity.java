912a8c55a430953b78f6c648bfe14d1622ea241f
Princie <princierakotoarivony4@gmail.com>
2022-05-12 14:38:50 +0300

    start btn

--- quiz/app/src/main/java/com/quiz/ui/SplashScreenActivity.java
+++ quiz/app/src/main/java/com/quiz/ui/SplashScreenActivity.java
@@ -2,7 +2,9 @@ package com.quiz.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
@@ -12,12 +14,14 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.airbnb.lottie.LottieAnimationView;
+import com.quiz.MainActivity;
 import com.quiz.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
-    Animation logo_animation;
+    Animation start_animation, logo_animation;
     ImageView logo_imageView;
+    Button start_btn;
     LottieAnimationView lottieAnimationView;
 
     @Override
@@ -26,12 +30,21 @@ public class SplashScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        start_animation = AnimationUtils.loadAnimation(this, R.anim.anim_bottom);
         logo_animation = AnimationUtils.loadAnimation(this, R.anim.anim_top);
+
         logo_imageView = findViewById(R.id.sp_logo);
+        start_btn = findViewById(R.id.start_btn);
 //        lottieAnimationView = findViewById(R.id.Logloading);
 
         logo_imageView.setAnimation(logo_animation);
+        start_btn.setAnimation(start_animation);
 //        lottieAnimationView.setVisibility(View.VISIBLE);
-
+        start_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
     }
 }
