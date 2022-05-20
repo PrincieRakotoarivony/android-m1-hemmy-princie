e63df031bff4a5ba5cf308d0d24b85542b3d55d4
Princie <princierakotoarivony4@gmail.com>
2022-05-20 21:48:06 +0300

    theme

--- quiz/app/src/main/java/com/quiz/ui/fragments/RankingFragment.java
+++ quiz/app/src/main/java/com/quiz/ui/fragments/RankingFragment.java
@@ -3,11 +3,14 @@ package com.quiz.ui.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
+import com.google.android.material.card.MaterialCardView;
 import com.quiz.R;
 
 public class RankingFragment extends Fragment {
@@ -17,6 +20,13 @@ public class RankingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_ranking, container, false);
+        MaterialCardView theme = root.findViewById(R.id.theme);
+        theme.setOnClickListener(new MaterialCardView.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(root).navigate(R.id.theme_list_to_details);
+            }
+        });
         return root;
     }
 }
---
+++ quiz/app/src/main/java/com/quiz/ui/fragments/ThemeFragment.java
@@ -0,0 +1,25 @@
+package com.quiz.ui.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.quiz.R;
+
+
+public class ThemeFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_theme, container, false);
+        return root;
+    }
+}
