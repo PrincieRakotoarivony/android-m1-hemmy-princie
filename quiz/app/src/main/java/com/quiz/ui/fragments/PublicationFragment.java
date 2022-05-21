7ff3046f9fb1b7424b8e20417580841549eec286
Princie <princierakotoarivony4@gmail.com>
2022-05-21 17:29:09 +0300

    comment

--- quiz/app/src/main/java/com/quiz/ui/fragments/PublicationFragment.java
+++ quiz/app/src/main/java/com/quiz/ui/fragments/PublicationFragment.java
@@ -1,5 +1,8 @@
 package com.quiz.ui.fragments;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,15 +13,35 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.quiz.R;
+
+import java.util.Objects;
 
 
 public class PublicationFragment extends Fragment {
+
+    Dialog commentDialog;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_publication, container, false);
+
+        commentDialog = new Dialog(getActivity(), R.style.AnimateDialog);
+        commentDialog.setContentView(R.layout.comment_popup);
+
+        FloatingActionButton commentBtn = root.findViewById(R.id.add_comment);
+        commentBtn.setOnClickListener(new FloatingActionButton.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Objects.requireNonNull(commentDialog.getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                commentDialog.show();
+            }
+        });
+
         return root;
     }
+
+
 }
