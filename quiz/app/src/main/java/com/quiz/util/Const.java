60fdfbf5282343806f82d75dce24d5a249a7a86a
Princie <princierakotoarivony4@gmail.com>
2022-05-23 21:50:35 +0300

    logo

--- quiz/app/src/main/java/com/quiz/util/Const.java
+++ quiz/app/src/main/java/com/quiz/util/Const.java
@@ -1,6 +1,8 @@
 package com.quiz.util;
 
 public class Const {
-    public static String BASE_URL = "http://192.168.1.159:3000";
+    //public static String BASE_URL = "http://192.168.1.159:3000";
+    //public static String BASE_URL = "http://192.168.203.169:3000";
+    public static String BASE_URL = "https://kidzy-back.herokuapp.com";
     public static String API_KEY = "AIzaSyArAUelnhfwOy9kg2sUr5Ya-pF-jHbpIYU";
 }
